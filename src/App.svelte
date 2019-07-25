afb6793ea4c9ba7c26217eab6a6a2971adabfbe5
Thiago Lagden <lagden@gmail.com>
2019-07-25 03:19:12 -0300

    move style to file

--- src/App.svelte
+++ src/App.svelte
@@ -9,24 +9,4 @@
 	{/each}
 </section>
 
-<style>
-@import './_assets/css/mixin.sss'
-
-.sample
-	&_container
-		padding: 1em
-		font-size: 1rem
-		font-family: "Helvetica Neue", sans-serif
-		width: 100%
-		max-width: 1200px
-		min-width: 360px
-		margin: 0 auto
-		box-sizing: border-box
-		display: grid
-		grid-gap: 1em
-		grid-template-columns: 1fr
-		@mixin mq $small
-			grid-template-columns: 1fr 1fr
-		@mixin mq $medium
-			grid-template-columns: 1fr 1fr 1fr 1fr
-</style>
+<style>@import './App.sss'</style>
